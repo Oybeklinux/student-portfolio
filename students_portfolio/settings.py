81e3671800dfb0c9c78a6356e250fca0bbede3eb
Oybeklinux <Oybeklinux@gmail.com>
2022-08-18 12:56:04 +0500

    static va media sozlamalari

--- students_portfolio/settings.py
+++ students_portfolio/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/static/' # deploy uchun
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'assets'),  # debug uchun assets
-#     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # deploy uchun
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),  # debug uchun assets
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = ""
+MEDIA_URL = "/media/"
 
 
 # Default v cvcnnnnnnnnnnnbbbbbbbbbb nnnnnnnnnnn  nhnvvvvvvvvvvvvvvvvvvvvvv gnnnnnnn bbbbbb ngffffffffx ZZZZZZZZZZZZZZZZ  primary key field type
